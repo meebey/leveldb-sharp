387c9aa5c9490c1fc861b93cc1b36a2b92827f4f
Mirco Bauer <meebey@meebey.net>
2012-03-08 22:15:27 +0100

    Added test-cases

---
+++ tests/NativeTests.cs
@@ -0,0 +1,112 @@
+// leveldb-sharp
+//
+// Copyright (c) 2012 Mirco Bauer <meebey@meebey.net>
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above
+//      copyright notice, this list of conditions and the following disclaimer
+//      in the documentation and/or other materials provided with the
+//      distribution.
+//    * Neither the name of Google Inc. nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace LevelDB
+{
+    [TestFixture]
+    public class NativeTests
+    {
+        IntPtr Database { get; set; }
+        string DatabasePath { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            var tempPath = Path.GetTempPath();
+            var randName = Path.GetRandomFileName();
+            DatabasePath = Path.Combine(tempPath, randName);
+            var options = Native.leveldb_options_create();
+            Native.leveldb_options_set_create_if_missing(options, true);
+            Database = Native.leveldb_open(options, DatabasePath);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (Database != IntPtr.Zero) {
+                Native.leveldb_close(Database);
+            }
+            if (Directory.Exists(DatabasePath)) {
+                Directory.Delete(DatabasePath, true);
+            }
+        }
+
+        [Test]
+        public void Open()
+        {
+            // NOOP, SetUp calls open for us
+        }
+
+        [Test]
+        public void Put()
+        {
+            var options = Native.leveldb_writeoptions_create();
+            Native.leveldb_put(Database, options, "key1", "value1");
+            Native.leveldb_put(Database, options, "key2", "value2");
+            Native.leveldb_put(Database, options, "key3", "value3");
+        }
+
+        [Test]
+        public void Get()
+        {
+            var options = Native.leveldb_readoptions_create();
+
+            Native.leveldb_put(Database, options, "key1", "value1");
+            var value1 = Native.leveldb_get(Database, options, "key1");
+            Assert.AreEqual("value1", value1);
+
+            Native.leveldb_put(Database, options, "key2", "value2");
+            var value2 = Native.leveldb_get(Database, options, "key2");
+            Assert.AreEqual("value2", value2);
+
+            Native.leveldb_put(Database, options, "key3", "value3");
+            var value3 = Native.leveldb_get(Database, options, "key3");
+            Assert.AreEqual("value3", value3);
+        }
+
+        [Test]
+        public void Delete()
+        {
+            var options = Native.leveldb_writeoptions_create();
+
+            Native.leveldb_put(Database, options, "key1", "value1");
+            var value1 = Native.leveldb_get(Database, options, "key1");
+            Assert.AreEqual("value1", value1);
+            Native.leveldb_delete(Database, options, "key1");
+            value1 = Native.leveldb_get(Database, options, "key1");
+            Assert.IsNull(value1);
+        }
+    }
+}
