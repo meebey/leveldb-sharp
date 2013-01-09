71c7d168377e8fbc0b7a09fffa858136b6d7ca14
Mirco Bauer <meebey@meebey.net>
2013-01-09 11:21:31 +0100

    Bumped copyright year of WriteOptions

--- WriteOptions.cs
+++ WriteOptions.cs
@@ -1,6 +1,6 @@
 //  leveldb-sharp
 // 
-//  Copyright (c) 2012, Mirco Bauer <meebey@meebey.net>
+//  Copyright (c) 2012-2013, Mirco Bauer <meebey@meebey.net>
 //  All rights reserved.
 // 
 //  Redistribution and use in source and binary forms, with or without
